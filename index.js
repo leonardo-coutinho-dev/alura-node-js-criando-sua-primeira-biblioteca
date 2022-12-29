494c632068a1c5b6a43f05a6945af4016330f4cf
leonardo <web.dev.trabalho@gmail.com>
2022-12-29 02:14:57 -0300

    capturing links from the file and showing on terminal

--- index.js
+++ index.js
@@ -22,19 +22,15 @@ import chalk from "chalk";
 //   chalk.cyanBright.bgGreen("Olá mundo, com esse texto agora em ciano!")
 // );
 
-// #2 TESTANDO O REGEX
-
-const textoTeste =
-  "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).";
+// #2 FUNÇÃO COM REGEX PARA FILTRAR O TEXTO
 
 let extraiLinks = (texto) => {
   const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.][^\s]*)\)/gm;
   const captura = [...texto.matchAll(regex)];
   const resultados = captura.map((item) => ({ [item[1]]: item[2] }));
   console.log(resultados);
+  return resultados;
 };
-
-extraiLinks(textoTeste);
 
 // #3 FUNÇÃO PARA TRATAMENTO DE ERRO
 
@@ -72,11 +68,12 @@ let pegaArquivo = async (caminhoDoArquivo) => {
   try {
     const encoding = "utf-8";
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(chalk.green(texto));
+    console.log(chalk.bgCyan(texto));
+    extraiLinks(texto);
   } catch (erro) {
     trataErro(erro);
   } finally {
-    console.log(chalk.blue("Execução finalizada!"));
+    console.log(chalk.green("Execução finalizada!"));
   }
 };
 
